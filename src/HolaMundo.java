8b274c0a28b13147163c5d27f60bbfd19a6569ba
jhonc <jhon_7246@hotmail.com>
2022-08-17 21:31:22 -0500

    Comentario de Jhon

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
     public static void main(String[] args) {
 
         System.out.println("Hello word!");
+        System.out.println("Hello word!");
 
         //Comentario de Steven, favor no borrar éste comentario
 
